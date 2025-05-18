b480784a0c68280a927423c569cb85aaf7be44f2
Hangang-png <ustb66@gmail.com>
2025-05-18 17:00:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-resume-react/',  // 替换成你的GitHub仓库名，注意前后斜杠
+  base: '/my-resume-react/', // 必须加上你的 GitHub 仓库名
   plugins: [react()],
 })
